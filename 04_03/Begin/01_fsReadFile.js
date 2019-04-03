5628361af879ac808a8635801c20791179edd288
Samson Akanbi <samson.akanbi@outlook.com>
2019-04-03 13:17:47 +0100

    Completed the second exercise in chapter 4 -3

--- 04_03/Begin/01_fsReadFile.js
+++ 04_03/Begin/01_fsReadFile.js
@@ -2,7 +2,11 @@
 // Chapter 04, Video 03, Exercise 01
 var fs = require('fs')
 
+var fileContents
+
 fs.readFile('message.txt', 'utf8', function(err, data) {
   if (err) throw err
-  console.log(data)
+  fileContents = data
+  console.log(fileContents)
 })
+
